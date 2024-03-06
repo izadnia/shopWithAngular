1711dcaf938785da77596b816ac54569177b0b65
Afshin Izadnia <izadnia@yahoo.com>
2024-03-07 01:48:20 +0330

    .

--- src/app/modules/checkout/checkout.module.ts
+++ src/app/modules/checkout/checkout.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { CheckoutComponent } from './checkout/checkout.component';
-import { PersianDatePipe } from '../../pipes/persiandate/persian-date.pipe';
 
 
 @NgModule({
   declarations: [
     CheckoutComponent,
-    PersianDatePipe
+
   ],
   imports: [
     CommonModule,
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -4,20 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductComponent } from './components/product/product.component';
-import { PersianDatePipe } from '../../pipes/persiandate/persian-date.pipe';
 import { SingleProductComponent } from './components/single-product/single-product.component';
 import { FormsModule } from '@angular/forms';
 import {FilterProductsPipe} from '../../pipes/filterproduct/filter-products.pipe'
-import { ProductService } from '../../services/product.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { environment } from '../../../environments/environment.development';
-import { ProductTestService } from '../../services/product-test.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     ProductsComponent,
     ProductComponent,
-    PersianDatePipe,
     SingleProductComponent,
     FilterProductsPipe
   ],
