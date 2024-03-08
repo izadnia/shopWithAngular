a69eac1525d56a3cf4a204e8af4a53d943167d58
Afshin Izadnia <izadnia@yahoo.com>
2024-03-09 02:08:23 +0330

    dashboard navbar updated

--- src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
+++ src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-navbar',
   templateUrl: './dashboard-navbar.component.html',
-  styleUrl: './dashboard-navbar.component.scss'
+  styleUrl: './dashboard-navbar.component.scss',
 })
 export class DashboardNavbarComponent {
-
+  fullUrl: any;
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.fullUrl = this.router.url.split('/').splice(2);
+      }
+    });
+  }
 }
