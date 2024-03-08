752b992abe22dcac4b283ff33dda5e0cd8f40a50
Afshin Izadnia <izadnia@yahoo.com>
2024-03-08 20:57:19 +0330

    chackout badge added to the header

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private productService: ProductService) {}
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getCheckoutList() 
+  }
+
+  checkoutList: Product[] = [];
+
+  getCheckoutList() {
+    this.productService.getSelectedProducts().subscribe((data) => {
+      this.checkoutList = data;
+    });
+  }
 }
