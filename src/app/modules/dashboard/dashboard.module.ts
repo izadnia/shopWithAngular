eee4956cc334e767c40ebff80294973ea455d1e7
Afshin Izadnia <izadnia@yahoo.com>
2024-03-08 23:10:12 +0330

    dashboard components created

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardSidebarComponent } from './dashboard/components/dashboard-sidebar/dashboard-sidebar.component';
+import { DashboardNavbarComponent } from './dashboard/components/dashboard-navbar/dashboard-navbar.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    DashboardSidebarComponent,
+    DashboardNavbarComponent
   ],
   imports: [
     CommonModule,
