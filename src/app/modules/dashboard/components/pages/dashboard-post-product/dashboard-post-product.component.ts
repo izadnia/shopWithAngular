2c5982120a68aa621610d541cf0b17017699b9f5
Afshin Izadnia <izadnia@yahoo.com>
2024-03-09 21:17:13 +0330

    post product ui updated

--- src/app/modules/dashboard/components/pages/dashboard-post-product/dashboard-post-product.component.ts
+++ src/app/modules/dashboard/components/pages/dashboard-post-product/dashboard-post-product.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Product } from '../../../../../models/product';
 
 @Component({
   selector: 'app-dashboard-post-product',
   templateUrl: './dashboard-post-product.component.html',
   styleUrl: './dashboard-post-product.component.scss'
 })
+
 export class DashboardPostProductComponent {
+
+
+  productModel : any ={
+    title: '',
+    price: undefined,
+    category: 'میوه',
+    expireDate: '1720068213258'
+  }
+
+
+
+  
 
 }
